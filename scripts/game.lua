18ed886cd1059dbe1fe44f88f687e5425bd57417
Andreas Tscheinig <andreas.tscheinig@dynatrace.com>
2023-04-14 11:56:47 +0200

    reformat code

--- scripts/game.lua
+++ scripts/game.lua
@@ -9,20 +9,20 @@ player.fall_direction = 1 -- 1 down, -1 up
 player.can_toggle = false
 player.last_time_grounded = 0
 
-map_pos=0
+map_pos = 0
 scroll_speed = 1
 
 -- this is basically _init
-function open_game() 
+function open_game()
 
     act_update = update_game
     act_draw = draw_game
 
-    last_time_grounded=t()-0.4
+    last_time_grounded = t() - 0.4
     camera(0, 0)
 
-    snow={}
-    for x=1,60 do
+    snow = {}
+    for x = 1, 60 do
         init_snowflake(x)
     end
 
@@ -33,54 +33,54 @@ function open_game()
     player.last_time_grounded = 0
 end
 
-function update_game() 
-	update_snow()
-  -- scroll map to the left
+function update_game()
+    update_snow()
+    -- scroll map to the left
     map_pos = map_pos - scroll_speed
 
-  if btnp(❎) and player.can_toggle then
-    toggle_gravity()
-  end
+    if btnp(❎) and player.can_toggle then
+        toggle_gravity()
+    end
 
-  move_player()
+    move_player()
 end
 
-function draw_game() 
-  -- clear screen
-  cls()
-  -- print fps
-  print(stat(7))
-  -- draw snow in background
-  draw_snow()
-  -- draw map
-  map(0, 0, map_pos, 0, 32, 12)
-  -- draw player sprite
-  local is_facing_down = player.fall_direction < 0
-  spr(2, player.x, player.y, 1, 1, false, is_facing_down)
+function draw_game()
+    -- clear screen
+    cls()
+    -- print fps
+    print(stat(7))
+    -- draw snow in background
+    draw_snow()
+    -- draw map
+    map(0, 0, map_pos, 0, 32, 12)
+    -- draw player sprite
+    local is_facing_down = player.fall_direction < 0
+    spr(2, player.x, player.y, 1, 1, false, is_facing_down)
 end
 
 function toggle_gravity()
-  -- reverse gravity
-  player.fall_direction = player.fall_direction * -1
-  -- timestamp since last grounded
-  player.last_time_grounded = t() - 0.4
-  -- can toggle gravity
-  player.can_toggle = false
+    -- reverse gravity
+    player.fall_direction = player.fall_direction * -1
+    -- timestamp since last grounded
+    player.last_time_grounded = t() - 0.4
+    -- can toggle gravity
+    player.can_toggle = false
 end
 
 function move_player()
-  local time_falling = t() - player.last_time_grounded
-  -- h = 1/2 * gravity * time^2
-  local y_offset = 1 / 2 * 4 * time_falling * time_falling * player.fall_direction
+    local time_falling = t() - player.last_time_grounded
+    -- h = 1/2 * gravity * time^2
+    local y_offset = 1 / 2 * 4 * time_falling * time_falling * player.fall_direction
 
-  player.y = player.y + y_offset
+    player.y = player.y + y_offset
 
-  collides_y(y_offset)
+    collides_y(y_offset)
 end
 
 function collides_y(offset_y)
-  local p_x_left = player.x + scroll_speed
-  local p_x_right = p_x_left + 8
-  local p_y_top = player.y + offset_y
-  local p_y_bottom = p_y_bottom
+    local p_x_left = player.x + scroll_speed
+    local p_x_right = p_x_left + 8
+    local p_y_top = player.y + offset_y
+    local p_y_bottom = p_y_bottom
 end
